80278a5553efa8bc059ffd6b25a1bb3a71077ea9
mohann12 <88677533+mohann12@users.noreply.github.com>
2026-08-21 00:17:17 +0530

    Update testsigma.sh

--- scripts/testsigma.sh
+++ scripts/testsigma.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+echo "Testsigma script has started"
+
 TEST_PLAN_ID="5398"
 
-echo "Testsigma script has started"
 echo "Test Plan ID: $TEST_PLAN_ID"
+echo "Starting Testsigma Test Plan..."
+
+curl -X POST \
+  -H "Content-type: application/json" \
+  -H "Accept: application/json" \
+  -H "Authorization: Bearer $TESTSIGMA_API_KEY" \
+  https://app.testsigma.com/api/v1/execution_results \
+  -d "{\"executionId\": \"$TEST_PLAN_ID\"}"
+
+echo
+echo "Testsigma execution request completed"
