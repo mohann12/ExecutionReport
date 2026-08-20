035e53c078959a98d7b078d1bf5b20d57fb20763
mohann12 <88677533+mohann12@users.noreply.github.com>
2026-08-21 01:53:33 +0530

    Increase wait time for Testsigma report update

--- scripts/testsigma.sh
+++ scripts/testsigma.sh
@@ -74,9 +74,9 @@ echo "Final Testsigma result: $FINAL_STATUS"
 # ==========================================
 
 echo
-echo "Waiting 30 seconds for Testsigma report data to be updated..."
+echo "Waiting 60 seconds for Testsigma report data to be updated..."
 
-sleep 30
+sleep 60
 
 # ==========================================
 # Generate PDF Report
